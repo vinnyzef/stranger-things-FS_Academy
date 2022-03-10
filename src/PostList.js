e5dcec4938d00027f144c6b322b475ddba8857b7
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2022-03-09 20:58:26 -0600

    end of class commit

--- src/PostList.js
+++ src/PostList.js
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { getPosts } from './api';
+import React, { useState, useEffect } from "react";
+import { getPosts } from "./api";
 
 const PostList = () => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(async () => {
-        const posts = await getPosts();
-        setPosts(posts);
-    }, []);
+  useEffect(async () => {
+    const p = await getPosts();
+    const posts = p.data.posts;
+    setPosts(posts);
+  }, []);
 
-    return (
-        <div>
-            {posts.map(post =>
-                <div key={post.id}>
-                    <h2>{post.title}</h2>
-                    <p>{post.body}</p>
-                </div>
-            )}
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default PostList;
--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 export const getPosts = async () => {
-    const url = 'https://jsonplaceholder.typicode.com/posts/';
-    const response = await fetch(url)
-    const json = await response.json()
-    return json;
-} 
+  const url =
+    "https://strangers-things.herokuapp.com/api/2112-ftb-et-web-pt/posts";
+  const response = await fetch(url);
+  const json = await response.json();
+  console.log(json);
+  return json;
+};
