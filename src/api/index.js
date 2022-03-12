88c68bb1ee9f43194a30f78cc9b9d6fd522a00bb
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2022-03-12 11:40:23 -0600

    user is logged in

--- src/api/index.js
+++ src/api/index.js
@@ -6,3 +6,41 @@ export const getPosts = async () => {
   console.log(json);
   return json;
 };
+
+export const registerUser = async (userObject) => {
+  const response = await fetch(
+    "https://strangers-things.herokuapp.com/api/2112-ftb-et-web-pt/users/register",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userObject),
+    }
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      localStorage.setItem("userKey", result.data.token);
+    })
+    .catch(console.error);
+};
+
+export const test = async () => {
+  const token = localStorage.getItem("userKey");
+  console.log(token);
+  const response = await fetch(
+    "https://strangers-things.herokuapp.com/api/2112-ftb-et-web-pt/test/me",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch(console.error);
+};
