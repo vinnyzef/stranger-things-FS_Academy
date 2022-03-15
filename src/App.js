337248af2a365021897e11193add90e400ffccb2
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2022-03-14 19:56:57 -0500

    testing setLoggedin(working)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, { useState } from "react";
 import { hot } from "react-hot-loader/root";
 import PostList from "./PostList";
 import Login from "./Login";
+import { useToken } from "./api";
+import SignUp from "./SignUp";
+
 
 const App = (props) => {
   const { name } = props;
   const [loggedIn, setLoggedIn] = useState(false);
+  const [token, setToken] = useState("");
+
   return (
     <>
-      {loggedIn ? null : (
-        <Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      {loggedIn ? (
+        <h1>Welcome, {}</h1>
+      ) : (
+        <Login
+          loggedIn={loggedIn}
+          setLoggedIn={setLoggedIn}
+          token={token}
+          setToken={setToken}
+        />
       )}
-
-      {loggedIn ? <h1>Welcome, {}</h1> : null}
       {loggedIn ? <PostList /> : null}
     </>
   );
--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,5 @@
+import { React, useState } from "react";
+
 export const getPosts = async () => {
   const url =
     "https://strangers-things.herokuapp.com/api/2112-ftb-et-web-pt/posts";
@@ -5,6 +7,25 @@ export const getPosts = async () => {
   const json = await response.json();
   console.log(json);
   return json;
+};
+export const loginUser = (userObject) => {
+  console.log(userObject);
+  fetch(
+    "https://strangers-things.herokuapp.com/api/2112-ftb-et-web-pt/users/login",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(userObject),
+    }
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch(console.error);
 };
 
 export const registerUser = async (userObject) => {
@@ -21,26 +42,49 @@ export const registerUser = async (userObject) => {
     .then((response) => response.json())
     .then((result) => {
       console.log(result);
-      localStorage.setItem("userKey", result.data.token);
+      localStorage.setItem("token", result.data.token);
     })
     .catch(console.error);
 };
 
 export const test = async () => {
-  const token = localStorage.getItem("userKey");
-  console.log(token);
+  const key = localStorage.getItem("token");
+
   const response = await fetch(
     "https://strangers-things.herokuapp.com/api/2112-ftb-et-web-pt/test/me",
     {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${key}`,
       },
     }
   )
     .then((response) => response.json())
     .then((result) => {
       console.log(result);
+
+      console.log(token);
     })
     .catch(console.error);
+};
+
+export const useToken = () => {
+  const getToken = () => {
+    const tokenString = localStorage.getItem("token");
+
+    const userToken = JSON.parse(tokenString);
+
+    //checks state to match token
+    return userToken?.token;
+  };
+  const [token, setToken] = useState(getToken());
+
+  const saveToken = (userToken) => {
+    localStorage.setItem("token", JSON.stringify(userToken));
+    setToken(userToken.token);
+  };
+  return {
+    setToken: saveToken,
+    token,
+  };
 };
